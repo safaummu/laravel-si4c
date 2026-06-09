3115985d2f598c0647db4ffd59743b04fdfe8e7a
safaummu <safaummunajwa_2428240131@mhs.mdp.ac.id>
2026-06-09 10:26:07 +0700

    otentikasi

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,18 +14,21 @@ class DashboardController extends Controller
      */
 public function index()
     {
-        $jumlahMahasiswa = DB::select('SELECT p.nama_prodi, COUNT(*) as jumlah 
-        FROM laravel.mahasiswas m 
-        JOIN laravel.prodis p 
-        on m.prodi_id = p.id 
-        GROUP BY p.nama_prodi');
+    $grafikmhs = DB::select("SELECT prodis.nama_prodi, 
+        COUNT(*) as jumlah_mhs 
+        FROM mahasiswas
+        JOIN prodis 
+        ON mahasiswas.prodi_id = prodis.id
+        GROUP BY prodis.nama_prodi");
 
-        $jumlahAngkatan = DB::select("SELECT CONCAT('20', SUBSTRING(npm, 1, 2)) as tahun_angkatan, COUNT(*) as jumlah 
-        FROM laravel.mahasiswas 
-        GROUP BY tahun_angkatan
-        ORDER BY tahun_angkatan ASC");
+        return view('dashboard-adminlte', compact('grafikmhs'));
 
-        return view('dashboard.index', compact('jumlahMahasiswa', 'jumlahAngkatan'));
+        // $jumlahAngkatan = DB::select("SELECT CONCAT('20', SUBSTRING(npm, 1, 2)) as tahun_angkatan, COUNT(*) as jumlah 
+        // FROM laravel.mahasiswas 
+        // GROUP BY tahun_angkatan
+        // ORDER BY tahun_angkatan ASC");
+
+        // return view('dashboard.index', compact('jumlahMahasiswa', 'jumlahAngkatan'));
     }
     
 }
